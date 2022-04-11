225979d28f4ede7d8148b1d1312a27c5756ccd2f
nina <blob@blub.com>
2022-04-11 15:27:22 +0200

    BUGFIX: Fixing Bug with spaces in paths

--- simulation_runner/run_basement/simulation.py
+++ simulation_runner/run_basement/simulation.py
@@ -10,10 +10,16 @@ from simulation_runner.run_basement.basement_scenario_configuration import (
 
 
 def _get_path_to_basement_simulation_executable(
-    basement_simulation_backend_configuration: BasementSimulationBackendConfiguration,
-) -> Path:
+        basement_simulation_backend_configuration: BasementSimulationBackendConfiguration,
+) -> str:
     root_path_to_basement_executables = GlobalConstants.root_path_to_basement_executables
-    return Path(root_path_to_basement_executables, basement_simulation_backend_configuration.value)
+    return handle_white_spaces_in_path(
+        Path(root_path_to_basement_executables, basement_simulation_backend_configuration.value)
+    )
+
+
+def handle_white_spaces_in_path(path: Path) -> str:
+    return rf'"{path.parent}\{path.parts[-1].split(" ")[0]}" {" ".join(path.parts[-1].split(" ")[1:])}'
 
 
 def simulate(basement_simulation_configuration: BasementScenarioConfiguration) -> None:
@@ -22,6 +28,6 @@ def simulate(basement_simulation_configuration: BasementScenarioConfiguration) -
     )
     shell_string_for_simulation = basement_simulation_configuration.to_shell_string_for_simulation()
     with change_back_to_original_wd_afterwards(basement_simulation_configuration.experiment_run_root_folder):
-        return_code = os.system(f'"{executable_path}" {shell_string_for_simulation} -p')
+        return_code = os.system(f'{executable_path} {shell_string_for_simulation} -p')
         if not return_code == 0:
             raise TypeError(f"{return_code=}")
