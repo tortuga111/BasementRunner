53be9df07cee0f5d4742e9293a5cac7fc6f6311a
Nina <nina.fluekiger@stud.unibas.ch>
2022-04-29 13:03:05 +0200

    new simulation configuration

--- simulation_configuration.py
+++ simulation_configuration.py
@@ -5,46 +5,54 @@ from simulation_runner.configurations.configuration_classes import (
 
 
 def get_experiment_base_run_root_folder():
-    return r"C:/Users/nina/Documents/04_Model_220321/04_Model/03_simulation_runs/07_calibration_runs_hw20_new_mesh/base_scenario"
+    return r"C:\Users\nina\Documents\04_Model_220427\04_Model\03_simulation_runs\01_calibration_run_with_new_mesh\base_scenario"
 
 
 def prepare_experiment_configurations():
     if use_discharge_file := True:
         discharge = ExperimentParameterToVary(
             pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/HYDRAULICS/BOUNDARY/STANDARD/0/discharge_file",
-            parameter_name="discharge_file",
+            parameter_name="DISF",
             values=[
-                r"C:/Users/nina/Documents/04_Model_220321/04_Model/01_input_data/Hydrographs/Hydrograph_HW2020.txt",
+                r"C:/Users/nina/Documents/04_Model_220427/04_Model/01_input_data/Hydrographs/HW20_112max.txt",
             ],
             json_file_name="model.json",
         )
     else:
         discharge = ExperimentParameterToVary(
             pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/HYDRAULICS/BOUNDARY/STANDARD/0/discharge",
-            parameter_name="discharge",
+            parameter_name="DIS",
             values=[
-                r"C:/Users/nina/Documents/04_Model_220321/04_Model/01_input_data/Hydrographs/SarineHydrograph_1_continue_HW2020_testshort.txt"
+                r"C:/Users/nina/Documents/04_Model_220321/04_Model/01_input_data/Hydrographs/SarineHydrograph_1_continue_HW2020_testshort.txt",
             ],
             json_file_name="model.json",
         )
 
-    simulation_time = ExperimentParameterToVary(
+
+    sim_time = ExperimentParameterToVary(
         pointer_to_location_in_json=r"/SIMULATION/TIME/end",
         parameter_name="end",
-        values=[900],
+        values=[111900],
         json_file_name="simulation.json",
     )
 
-    bed_load_inflow = ExperimentParameterToVary(
-        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/MORPHOLOGY/BEDLOAD/BOUNDARY/STANDARD",
-        parameter_name="bed_load_inflow",
-        values=[
-            [
-                {"boundary_factor": 1, "name": "input", "string_name": "input", "type": "transport_capacity"},
-                {"boundary_factor": 1, "name": "output", "string_name": "output", "type": "transport_capacity"},
-            ],
-        ],
+    critical_angle_dry = ExperimentParameterToVary(
+        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/MORPHOLOGY/GRAVITATIONAL_TRANSPORT/critical_angle_dry",
+        parameter_name="CAD",
         json_file_name="model.json",
+        values=[60.0],
+    )
+    critical_angle_wet = ExperimentParameterToVary(
+        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/MORPHOLOGY/GRAVITATIONAL_TRANSPORT/critical_angle_wet",
+        parameter_name="CAW",
+        json_file_name="model.json",
+        values=[40],
+    )
+    repose_angle = ExperimentParameterToVary(
+        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/MORPHOLOGY/INCIPIENT_MOTION/repose_angle",
+        parameter_name="repose_angle",
+        json_file_name="model.json",
+        values=[40],
     )
 
     fixed_bed = ExperimentParameterToVary(
@@ -53,15 +61,41 @@ def prepare_experiment_configurations():
         json_file_name="model.json",
         values=[
             [
-                {"region_name": "one", "z_rel": 0.0},
-                {"region_name": "two", "z_rel": 0.0},
-                {"region_name": "three", "z_rel": 0.0},
-                {"region_name": "four", "z_rel": 0.0},
-                {"region_name": "five", "z_rel": 0.0},
+                {"region_name": "one", "z_rel": -0.2},
+                {"region_name": "two", "z_rel": -0.5},
+                {"region_name": "three", "z_rel": -0.2},
+                {"region_name": "four", "z_rel": -0.5},
+                {"region_name": "five", "z_rel": -0.1},
                 {"region_name": "six", "z_rel": 0.0},
-                {"region_name": "seven", "z_rel": 0.0},
-                {"region_name": "eight", "z_rel": 0.0},
-            ]
+                {"region_name": "seven", "z_rel": -0.5},
+                {"region_name": "eight", "z_rel": -0.0},
+            ],
+            [
+                {"region_name": "one", "z_rel": -0.5},
+                {"region_name": "two", "z_rel": -0.5},
+                {"region_name": "three", "z_rel": -0.5},
+                {"region_name": "four", "z_rel": -0.5},
+                {"region_name": "five", "z_rel": -0.1},
+                {"region_name": "six", "z_rel": 0.0},
+                {"region_name": "seven", "z_rel": -0.5},
+                {"region_name": "eight", "z_rel": -0.0},
+            ],
         ],
     )
-    return ExperimentParameterConfigurator([discharge, simulation_time, bed_load_inflow, fixed_bed])
+
+    friction = ExperimentParameterToVary(
+        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/HYDRAULICS/FRICTION/default_friction",
+        parameter_name="k",
+        values=[30.4],
+        json_file_name="model.json",
+    )
+
+    grain_size = ExperimentParameterToVary(
+        pointer_to_location_in_json=r"/SETUP/DOMAIN/BASEPLANE_2D/MORPHOLOGY/BEDMATERIAL/GRAIN_CLASS/diameters",
+        parameter_name="grain_size",
+        json_file_name="model.json",
+        values=[[0.082]],
+    )
+    return ExperimentParameterConfigurator(
+        [discharge, sim_time, critical_angle_wet, critical_angle_dry, repose_angle, fixed_bed, grain_size, friction]
+    )
